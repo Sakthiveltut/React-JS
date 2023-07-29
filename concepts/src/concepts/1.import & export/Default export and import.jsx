79401b9cf953f3af051457555f291c8d0da9ce69
sakthivetuty <msakthiveltut@gmail.com>
2023-07-29 23:18:51 +0530

    router

--- concepts/src/concepts/1.import & export/Default export and import.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Display1(){
-    return(<><h3>1.Default export and import</h3></>)
-}
-
-export default Display1;
--- concepts/src/concepts/1.import & export/Named export and import.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Display2(){
-    return(<><h3>2.Named export and import</h3></>)
-}
