94bacfeba52cae13e92592178582592fd0334cab
sakthivetuty <msakthiveltut@gmail.com>
2023-09-02 18:38:08 +0530

    quiz

---
+++ quiz/src/components/questions.js
@@ -0,0 +1,27 @@
+export default[
+    {
+        question:1,
+        options:[1,2,3,4],
+        answer:0
+    },
+    {
+        question:2,
+        options:[1,2,3,4],
+        answer:0
+    },
+    {
+        question:3,
+        options:[1,2,3,4],
+        answer:0
+    },
+    {
+        question:4,
+        options:[1,2,3,4],
+        answer:0
+    },
+    {
+        question:5,
+        options:[1,2,3,4],
+        answer:0
+    },
+]
